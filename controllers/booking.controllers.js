3500ed6af147ad1e08492c7cecb0c20972c5d3aa
SauloGD <saulogardo@gmail.com>
2021-06-04 15:06:21 +0100

    some fix

--- controllers/booking.controllers.js
+++ controllers/booking.controllers.js
@@ -12,6 +12,18 @@ function createBooking(req,res){
     })
 }
 
+
+function updateBooking(req,res){
+    bookingModel.findByIdAndUpdate(req.params.bookingId, req.body, { new: true })
+    .then ((booking) => {
+        res.json(booking)
+    })
+    .catch((err) => {
+        res.json(err)
+    })
+}
+
+
 function deleteBooking(req,res){
     bookingModel.findByIdAndDelete (req.params.bookingId)
     .then ((booking) => {
@@ -49,10 +61,18 @@ function getAllBookingByAdmin (req, res){
 
 function getOneBookingByUser (req, res){
     bookingModel.findById(req.params.bookingId)
-    
+    .populate("establishmentId")
     .then ((booking) => {
+        var plant = {
+            "_id" : booking._id,
+            "establishmentName" : booking.establishmentId.establishmentName,
+            "categoryType" : booking.establishmentId.category[0].categoryType,
+            "date" : booking.date,
+            "covidTest" : booking.covidTest,
+            "checkInComplete" : booking.checkInComplete
+        }
        if(booking.userId._id.toString() === res.locals.id){
-        res.json(booking)
+        res.json(plant)
        }else{
         res.json("La reserva que buscas no esta a tu nombre")
        }
@@ -64,9 +84,24 @@ function getOneBookingByUser (req, res){
 
 function getAllBookingByUser (req, res){
     bookingModel.find({userId : res.locals.id})
+    .populate("establishmentId")
     .then ((booking) => {
-       res.json(booking)
+        var result = []
+        booking.forEach((elem) => {
+        var plant = {
+            "_id" : elem._id,
+            "establishmentName" : elem.establishmentId.establishmentName,
+            "categoryType" : elem.establishmentId.category[0].categoryType,
+            "date" : elem.date,
+            "covidTest" : elem.covidTest,
+            "checkInComplete" : elem.checkInComplete
+        }
+        result.push(plant)
     })
+
+    res.json(result)
+        })
+    
     .catch((err) => {
         res.json(err)
     })
@@ -78,9 +113,22 @@ function getAllBookingByUser (req, res){
 
 function filterUserBookingByDateandCheckInComplete (req, res){
     bookingModel.find({userId : res.locals.id , date : {$eq : req.query.date} , checkInComplete : {$eq : req.query.checkInComplete}})
+    .populate("establishmentId")
     .then ((booking) => {
-       res.json(booking)
+        var result = []
+        booking.forEach((elem) => {
+        var plant = {
+            "_id" : elem._id,
+            "establishmentName" : elem.establishmentId.establishmentName,
+            "categoryType" : elem.establishmentId.category[0].categoryType,
+            "date" : elem.date,
+            "covidTest" : elem.covidTest,
+            "checkInComplete" : elem.checkInComplete
+        }
+        result.push(plant)
     })
+    res.json(result)
+        })
     .catch((err) => {
         res.json(err)
     })
@@ -88,13 +136,26 @@ function filterUserBookingByDateandCheckInComplete (req, res){
 
 function filterUserBookingByDate (req, res){
     bookingModel.find({userId : res.locals.id , date : req.query.date})
+    .populate("establishmentId")
     .then ((booking) => {
-       res.json(booking)
+        var result = []
+        booking.forEach((elem) => {
+        var plant = {
+            "_id" : elem._id,
+            "establishmentName" : elem.establishmentId.establishmentName,
+            "categoryType" : elem.establishmentId.category[0].categoryType,
+            "date" : elem.date,
+            "covidTest" : elem.covidTest,
+            "checkInComplete" : elem.checkInComplete
+        }
+        result.push(plant)
     })
+    res.json(result)
+        })
     .catch((err) => {
         res.json(err)
     })
 }
 
 
-module.exports = {createBooking, deleteBooking, getOneBookingByAdmin, getAllBookingByAdmin, getOneBookingByUser, getAllBookingByUser, filterUserBookingByDateandCheckInComplete, filterUserBookingByDate}
+module.exports = {createBooking, deleteBooking, getOneBookingByAdmin, getAllBookingByAdmin, getOneBookingByUser, getAllBookingByUser, filterUserBookingByDateandCheckInComplete, filterUserBookingByDate, updateBooking}
